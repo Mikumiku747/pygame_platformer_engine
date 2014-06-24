d58813361ff567e052d66800a44363584e2d079c
Mikumiku747 <Mikumiku747@gmail.com>
2014-06-24 16:01:24 +1000

    Initial publish
    This is just to get the project up and running. It's my first time using
    git, so sorry if I make any silly mistakes.

---
+++ main.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        main
+# Purpose:     Contains the main game loop, game logic, that kinda thing. May be
+#              changed later to only include the main game loop and central
+#              logic if possible.
+#
+# Author:      Daniel Selmes
+#
+# Created:     24/06/2014
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
