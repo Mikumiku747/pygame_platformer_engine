0777c92fd7485de18913338e2b169bdc1842aae4
Mikumiku747 <Mikumiku747@gmail.com>
2014-09-17 16:35:56 +1000

    Started the animation
    Should make a pull request soon, this isn't really part of the redesign

---
+++ animation.py
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+# Name:        animation
+# Purpose:     To provide a library to easily animate a sprite. Also serves to
+#               easily load in images and group them.
+#
+# Author:      Daniel
+#
+# Created:     17/09/2014
+# Version:     1.0
+# Copyright:   (c) Daniel 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#Imports
+import pygame
+pygame.init()
+
+from os import
+load = pygame.image.load
+
+def load_folder(folder_path):
+    "Loads a bunch of images from a folder, returns a list of surfaces."
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
